65add51c7cce0e282594c6d9bbb646f25eb8eac3
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-10-02 15:49:15 -0400

    Added shuffling tunctionality to Deck

--- src/Deck.js
+++ src/Deck.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect, useRef} from "react";
 import axios from "axios";
+import "./Deck.css"
 
 function Deck() {
     const initialDrawn = []
     const [drawn, setDrawn] = useState(initialDrawn)
+    const [isShuffling, setIsShuffling] = useState(false)
+    const [isDrawing, setIsDrawing] = useState(false)
     const deckId = useRef()
 
     useEffect(function initializeDeck() {
@@ -15,11 +18,13 @@ function Deck() {
     }, [])
 
     async function drawCard() {
+        setIsDrawing(true)
         const deckResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=1`)
         const draw = deckResponse.data
         if(draw.success) {
             const card = draw.cards[0]
-            setDrawn([...drawn, {code: card.code, image: card.image}])
+            const newCard = {code: card.code, image: card.image, value: card.value, suit: card.suit}
+            setDrawn([...drawn, newCard])
         } else {
             if(draw.remaining === 0){
                 alert("Error: no cards remaining!")
@@ -27,16 +32,29 @@ function Deck() {
                 alert("An error has occured with the api, please try again later")
             }
         }
+        setIsDrawing(false)
+    }
+
+    async function shuffleDeck() {
+        setIsShuffling(true)
+        await axios.get(`https://deckofcardsapi.com/api/deck/${deckId.current}/shuffle`)
+        setDrawn(initialDrawn)
+        setIsShuffling(false)
     }
 
     return(
         <div className="deck">
-            <button onClick={drawCard}>Draw Card</button>
+            <h1>Deck of Cards</h1>
+            {!isDrawing && <button onClick={drawCard}>Draw Card</button>}
+            {isDrawing && <b>Please Wait</b>}
+            <hr />
             <div className="drawn">
                 {drawn.map((card) => 
-                  <img key={card.code} src={card.image}/>
+                  <img className="card" alt={`${card.value} OF ${card.suit}`} key={card.code} src={card.image}/>
                 )}
             </div>
+            <hr />
+            {!isShuffling && <button onClick={shuffleDeck}>Shuffle Deck</button>}
         </div>
     )
 }
